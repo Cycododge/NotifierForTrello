c031be2703385e14c3ad5f583c2d9b877d156f2a
Cycododge <cycododge@server.fake>
2013-07-20 00:58:23 -0700

    Added settings menu

--- Chrome/v2/background.js
+++ Chrome/v2/background.js
@@ -1,3 +1,20 @@
+/*
+License: http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
+  _____                        _             _
+ / ____|                      | |           | |
+| |     _   _   ___  ____    _| |  ____    _| |  __ _   ___
+| |    | | | | / __|/ _  \ / _` | / _  \ / _` | / _` | / _ \
+| |____| |_| || (__| (_) || (_| || (_) || (_| || (_| ||  __/
+\_____|\__,_ |\___|\____/ \__,__|\____/ \__,__|\__, | \___|
+         __/ |                                 __/ |
+        |___/                                 |___/
+
+Build: 2.0.0
+Date: 7/20/2013
+http://cycododge.com
+http://twitter.com/cycododge
+*/
+
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
 // var settings = new Store("settings", {
--- Chrome/v2/js/popup.js
+++ Chrome/v2/js/popup.js
@@ -1,4 +1,20 @@
-/* 7/17/2013 */
+/*
+License: http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
+  _____                        _             _
+ / ____|                      | |           | |
+| |     _   _   ___  ____    _| |  ____    _| |  __ _   ___
+| |    | | | | / __|/ _  \ / _` | / _  \ / _` | / _` | / _ \
+| |____| |_| || (__| (_) || (_| || (_) || (_| || (_| ||  __/
+\_____|\__,_ |\___|\____/ \__,__|\____/ \__,__|\__, | \___|
+         __/ |                                 __/ |
+        |___/                                 |___/
+
+Build: 2.0.0
+Date: 7/20/2013
+http://cycododge.com
+http://twitter.com/cycododge
+*/
+
 (function($){
 	/* INITIALIZE */
 
@@ -16,11 +32,21 @@
 		$(this).addClass('active');
 	});
 
-	//settings menu
+	//user menu
 	$('.btnUserMenu').on('mouseenter',function(){
 		$('#userMenu').slideDown(100);
 	}).on('mouseleave',function(){
 		$('#userMenu').slideUp();
+	});
+
+	//open settings menu
+	$('#userMenu .btnSettings').on('click',function(){
+		$('#settings').show(500);
+	});
+
+	//close settings menu
+	$('#settings .header').on('click',function(){
+		$('#settings').hide(500);
 	});
 
 	/* FUNCTIONS */
