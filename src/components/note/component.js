1a47bcbbfe0f3ec82b3e594662eb4aeff5030843
Cycododge <cycododge@media-center.com>
2017-02-25 16:41:11 -0700

    formatting for known notification types

--- src/components/note/component.js
+++ src/components/note/component.js
@@ -15,6 +15,49 @@
 	function controller() {
 		//config
 		var vm = this;
+		vm.noteTypes = {
+			commentCard: '<user-link user="vm.note.memberCreator"></user-link> commented on the card [card_name] on [board_name]',
+
+			memberJoinedTrello: '<user-link user="vm.note.memberCreator"></user-link> joined Trello on your recommendation. You earned a free month of Trello Gold',
+
+			removedFromCard: '<user-link user="vm.note.memberCreator"></user-link> removed you from the card [card_name] on [board_name]',
+
+			addedToCard: '<user-link user="vm.note.memberCreator"></user-link> added you to the card [card_name] on [board_name]',
+
+			mentionedOnCard: '<user-link user="vm.note.memberCreator"></user-link> mentioned you on the card [card_name] on [board_name] [mention]',
+
+			changeCard: '<user-link user="vm.note.memberCreator"></user-link> moved the card [card_name] to list_name on [board_name]',
+
+			createdCard: '<user-link user="vm.note.memberCreator"></user-link> created [card_name] in list_name on [board_name]',
+
+			updateCheckItemStateOnCard: '<user-link user="vm.note.memberCreator"></user-link> checked [checked] on [card_name] on [board_name]',
+
+			addedMemberToCard: '<user-link user="vm.note.memberCreator"></user-link> joined the card [card_name] on [board_name]',
+
+			removedMemberFromCard: '<user-link user="vm.note.memberCreator"></user-link> left the card [card_name] on [board_name]',
+
+			addedAttachmentToCard: '<user-link user="vm.note.memberCreator"></user-link> attached [attached] to [card_name] on [board_name]',
+
+			addedToBoard: '<user-link user="vm.note.memberCreator"></user-link> added you to the board [board_name]',
+
+			removedFromBoard: '<user-link user="vm.note.memberCreator"></user-link> removed you from the board [board_name]',
+
+			invitedToBoard: '<user-link user="vm.note.memberCreator"></user-link> invited you to the board [board_name]',
+
+			addAdminToBoard: '<user-link user="vm.note.memberCreator"></user-link> made you a co-owner on the board [board_name]',
+
+			makeAdminOfBoard: '<user-link user="vm.note.memberCreator"></user-link> made you a co-owner on the board [board_name]',
+
+			closeBoard: '<user-link user="vm.note.memberCreator"></user-link> closed the board [board_name]',
+
+			removedFromOrganization: '<user-link user="vm.note.memberCreator"></user-link> removed you from the organization [organization]',
+
+			invitedToOrganization: '<user-link user="vm.note.memberCreator"></user-link> invited you to the organization [organization]',
+
+			addAdminToOrganization: '<user-link user="vm.note.memberCreator"></user-link> made you an admin on the organization [organization]',
+
+			makeAdminOfOrganization: '<user-link user="vm.note.memberCreator"></user-link> made you an admin of the organization [organization]'
+		};
 		vm.fn = {
 		};
 
