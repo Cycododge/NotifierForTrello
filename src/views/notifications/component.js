c6de56bbcae3afd72b43337134b15470bf9084f2
Cycododge <cycododge@server.fake>
2017-03-23 22:13:58 -0700

    allow filtering by user

--- src/views/notifications/component.js
+++ src/views/notifications/component.js
@@ -27,6 +27,7 @@
 				filter: { unread: true }
 			}
 		];
+		vm.people = {};
 
 		//perform initial setup
 		init();
@@ -42,6 +43,26 @@
 				success: function(){
 					//get the note data
 					vm.notes = bkg.note_data;
+					console.log(vm.notes);
+
+					//create dynamic people filters
+					populatePeopleFilters(vm.notes);
+				}
+			});
+		}
+
+		//Create filters for people who created notifications
+		function populatePeopleFilters(notes){
+			//loop through each note
+			notes.forEach(function(note){
+				//if a person created this note AND they aren't already added
+				if(note.memberCreator && !vm.people[note.memberCreator.id]){
+					//cache this person
+					vm.people[note.memberCreator.id] = {
+						id: note.memberCreator.id,
+						name: note.memberCreator.fullName,
+						filter: { idMemberCreator: note.memberCreator.id }
+					};
 				}
 			});
 		}
