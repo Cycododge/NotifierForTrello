78b2bc269c64215399ab7a33760a7588ba3f3860
Cycododge <cycododge@server.fake>
2017-03-30 20:40:17 -0700

    open all links in a new tab

--- src/directives/external/directive.js
+++ src/directives/external/directive.js
@@ -26,20 +26,59 @@
 
 		//send user to an external link
 		function goToUrl(URL){
+			//open the link in a new tab
+			chrome.tabs.create({ url: URL });
+
+
+			////////////////////////////////
+			// Below is pending work/brainstorming for Smart Tabs
+			////////////////////////////////
+
+
 			//The Trello URL to check against
-			var urlCheck = 'trello.com';
+			// var urlCheck = 'trello.com';
+
+			//find all open Trello tabs
+			// chrome.tabs.query({ url: 'https://trello.com/*' }, function(tabs){
+			// 	console.log(tabs);
+			// });
 
 			//find the current tab
-			chrome.tabs.getSelected(null, function(tab){
-				//if the current tab is on the correct site AND the URL matches too
-				if(tab.url.indexOf(urlCheck) > -1 && URL.indexOf(urlCheck) > -1){
+			// chrome.tabs.getSelected(null, function(tab){
+				//"Open Trello Links in a New Tab"
+				//// "Hold CTRL to toggle behavior"
+				//alwaysNewTab = false;
+				//viewingTrello = false;
+
+				//if alwaysNewTab
+					//new tab
+				//if !alwaysNewTab
+					//if linking to trello
+						//replace current tab
+					//if !viewingTrello
+						//new tab
+
+				//viewing Trello website
+					//if alwaysNewTab
+						//new tab
+					//if !alwaysNewTab
+						//if linking to trello
+						//if !linking to trello
+				//not viewing Trello
+					//if new tab page
+						//current tab
+					//if !new tab page
+						//new tab
+
+				//if the current tab is Trello AND the URL matches too
+				// if(tab.url.indexOf(urlCheck) > -1 && URL.indexOf(urlCheck) > -1){
 					//go to this url via the current tab
-					chrome.tabs.update(tab.id,{ url: URL });
-				}else{
+					// chrome.tabs.update(tab.id,{ url: URL });
+				// }else{
 					//open a new tab
-					chrome.tabs.create({ url: URL });
-				}
-			});
+					// chrome.tabs.create({ url: URL });
+				// }
+			// });
 		}
 	}
 }());
